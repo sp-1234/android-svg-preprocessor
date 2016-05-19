72d38d659885374de3109847bc1f17fdcc4e702e
Sergei Pavlov <sp@wisebits.com>
2016-05-19 16:43:50 +0300

    done

--- android-svg-preprocessor.py
+++ android-svg-preprocessor.py
@@ -5,13 +5,24 @@ from os import path
 import shlex
 import shutil
 import argparse
+from shlex import quote
 
 DEFAULT_TARGET_PATH = os.getcwd()
+DEFAULT_PREFIX = 'drawable'
+
+DPIS = {
+    'xxxhdpi': 4,
+    'mdpi': 1,
+    'hdpi': 1.5,
+    'xhdpi': 2,
+    'xxhdpi': 3,
+    'tvdpi': 1.33
+}
 
 
-def sh(command: str):
+def sh(command):
     import subprocess as sp
-    return_code = sp.Popen(command, shell=True).wait()
+    return_code = sp.Popen(command).wait()
     assert return_code == 0
 
 
@@ -20,15 +31,57 @@ def parse_args():
     ap.add_argument('--path', '-p', default=DEFAULT_TARGET_PATH,
                     help='the resources path, default is %s' % DEFAULT_TARGET_PATH)
 
+    ap.add_argument('--prefix', default=DEFAULT_PREFIX,
+                    help='the resources path, default is %s' % DEFAULT_TARGET_PATH)
+
     ap.add_argument('input_files', nargs='+', help='input images (SVG)')
     return ap.parse_args()
+
+
+def basename_no_ext(full_path):
+    return os.path.splitext(os.path.basename(full_path))[0]
+
+
+def process_file_for_dpi(dpi_name, dpi_scale, input_file, output_path, prefix):
+    resource_name = basename_no_ext(input_file)
+    output_path = os.path.join(
+        output_path,
+        "{}-{}".format(prefix, dpi_name)
+    )
+    os.makedirs(output_path, exist_ok=True)
+    output_file_path = os.path.join(
+        output_path,
+        resource_name + ".png"
+    )
+    density_percent = 100.0 * dpi_scale
+    cmd = [
+        'convert',
+        '-density', '{}%'.format(density_percent),
+        '-background', 'none',
+        input_file,
+        '-colorspace', 'srgb',
+        '-depth', '8',
+        output_file_path
+    ]
+    sh(cmd)
+
+
+def process_file(input_file, output_path, prefix):
+    for dpi_name, dpi_scale in DPIS.items():
+        process_file_for_dpi(dpi_name, dpi_scale, input_file, output_path, prefix)
+
+
+def do_process(inputs, output_path, prefix):
+    for input_file in inputs:
+        process_file(input_file, output_path, prefix)
 
 
 def main():
     ap = parse_args()
     inputs = ap.input_files
     output_path = ap.path
-    print(inputs)
+    prefix = ap.prefix
+    do_process(inputs, output_path, prefix)
 
 
 if __name__ == '__main__':
